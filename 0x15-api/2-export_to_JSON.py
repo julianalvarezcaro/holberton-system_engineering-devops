ffb1ca39b4793d59f14cffc5417e978aaf8d1614
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-02 01:32:40 +0000

    Completed task 2. Writes a json format file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Export data in the JSON format.
+"""
+
+import csv
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user_id = argv[1]
+
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'
+    resp_todos = requests.get(url_todo.format(user_id))
+    resp_user = requests.get(url_user.format(user_id))
+    json_r_todos = resp_todos.json()
+    json_r_user = resp_user.json()
+
+    user_name = json_r_user.get("username")
+
+    json_fname = user_id + ".json"
+
+    val_l = []
+    for task in json_r_todos:
+        del task["userId"]
+        del task["id"]
+        val_l.append(task)
+    dic = {str(user_id): val_l}
+
+    with open(json_fname, 'w+') as fd_json:
+        data = json.dumps(dic)
+        fd_json.write(data)
