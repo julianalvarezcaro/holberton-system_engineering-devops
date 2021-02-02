27e0578499effa37c189c566a7843a10ed24f772
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-02 00:39:36 +0000

    Completed task 2. Creates a file with CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    user_id = argv[1]
+
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'
+    resp_todos = requests.get(url_todo.format(user_id))
+    resp_user = requests.get(url_user.format(user_id))
+    json_r_todos = resp_todos.json()
+    json_r_user = resp_user.json()
+
+    user_name = json_r_user.get("name")
+
+    csv_fname = user_id + ".csv"
+    fd_csv = open(csv_fname, 'w+')
+
+    # Object used to write in CSV format
+    csv_writer = csv.writer(fd_csv)
+
+    for task in json_r_todos:
+        csv_writer.writerow([user_id,
+                            user_name, task['completed'], task['title']])
